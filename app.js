c5755a746b91ae57c26c4cad7535ad56b27aa394
ghanshyamtanchak <ghtanchak77@gmail.com>
2023-02-28 20:50:03 +0530

    404 page

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-app.use(adminRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("In another middleware!");
   res.send("<h1>Hello from Express</h1>");
 });
 
